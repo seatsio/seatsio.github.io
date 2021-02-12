ed1941b71cffb2ded5debf59307420474feaaaa4
Ben Verbeken <ben@seats.io>
2021-02-12 13:53:29 +0100

    edited docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   title: 'My Site',
   tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
+  url: 'https://seatsio.github.io',
   baseUrl: '/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'seatsio', // Usually your GitHub org/user name.
+  projectName: 'seatsio.github.io', // Usually your repo name.
   themeConfig: {
     navbar: {
       title: 'My Site',
