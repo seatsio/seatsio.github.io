fd7854a01b6bb29c4a4bacdc18ef48b44121f9e9
Ben Verbeken <ben@seats.io>
2021-02-15 13:35:36 +0100

    hideable sidebar + removed edit links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -40,17 +40,15 @@ module.exports = {
     },
     colorMode: {
       disableSwitch: true,
-    }
+    },
+    hideableSidebar: true,
   },
   presets: [
     [
       '@docusaurus/preset-classic',
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+          sidebarPath: require.resolve('./sidebars.js')
         },
         theme: {
           customCss: require.resolve('./src/css/custom.css'),
